ccd8b9c1bb29e48832cfa5dc631db7ce462ae2a0
Muneeb Ahmad <100162863+Muneeb233@users.noreply.github.com>
2022-09-16 02:47:05 +0500

    backend ready apis working

--- routes/notes.js
+++ routes/notes.js
@@ -13,7 +13,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
         console.error(error.message);
         res.status(500).send("Interval server error occured");
     }
-    
+
 })
 
 
@@ -40,4 +40,54 @@ router.post('/addnote', fetchuser, [
 
     })
 
+//route3 update all existing notes with put request 
+
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+    const { title, description, tag } = req.body;
+    try {
+        //creta a new note object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+        //find note to update find and update
+        let note = await Note.findById(req.params.id)
+        if (!note) { return res.status(404).send("Not Found ") }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed ");
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Interval server error occured");
+    }
+
+})
+
+
+//route4 Delete an existing Note using DELETE with a user id 
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+
+    try {
+
+
+        //find note to update and delete it 
+        let note = await Note.findById(req.params.id)
+        if (!note) { return res.status(404).send("Not Found ") }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed ");
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Sucess": "Note has been deleted ", note: note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Interval server error occured");
+    }
+})
 module.exports = router
