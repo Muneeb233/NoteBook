af818a79b995e6e64bd33f287df5f22b9f6b92d1
Muneeb Ahmad <100162863+Muneeb233@users.noreply.github.com>
2022-09-15 13:59:58 +0500

    end points added

--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt=require('bcryptjs');
 var jwt=require('jsonwebtoken');
+var fetchuser=require('../middleware/fetchuser'); 
 const JWT_SECRET='ILOVEREACT@';
 
-//Creating a userusing POST"api/auth/createuser" i does not require authenticaiton
+//Route:1 Creating a userusing POST"api/auth/createuser" i does not require authenticaiton
 router.post('/createuser', [
     body('name', 'enter a valid name').isLength({ min: 3 }),
     body('email', 'enter a valid email').isEmail(),
@@ -44,11 +45,56 @@ router.post('/createuser', [
         res.json({authToken});
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("Some error occured");
+        res.status(500).send("Interval server error occured");
     }
 
 
 })
 
+//Route:2 authenticate a user userusing POST"api/auth/login" no login required
+router.post('/login', [
+    body('email', 'enter a valid email').isEmail(),
+    body('password', 'password can not be blank').exists(),
 
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email,password}=req.body;
+    try{
+        let user= await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Login with correct credentials"})
+        }
+        const passwordCompare= await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Login with correct credentials"})
+
+        }
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+    const authToken= jwt.sign(data,JWT_SECRET);
+    res.json({authToken});
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send("Interval server error occured");
+    }
+})
+//Route:3 get logged in user details using : /api/auth/getuser
+router.post('/getuser',fetchuser, async (req, res) => {
+try {
+userId=req.user.id;
+    const user=await User.findById(userId).select("-password");   
+    res.send(user);
+}   catch (error) {
+    console.error(error.message);
+    res.status(500).send("Interval server error occured");
+} 
+})
 module.exports = router
--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', (req, res) => {
+const fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator');
 
+//route1 get all the notes
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id })
+        res.json(notes)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Interval server error occured");
+    }
     
-    res.json([])
-
 })
 
+
+//route2  Add all notes with post request 
+router.post('/addnote', fetchuser, [
+    body('title ', 'enter a valid title'),
+    body('description', 'enter a valid description').isLength({ min: 5 }),], async (req, res) => {
+        try {
+            const { title, description, tag } = req.body;
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save()
+            res.json(savedNote)
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Interval server error occured");
+        }
+
+    })
 
 module.exports = router
